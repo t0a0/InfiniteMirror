9db9c3288c62f734a52f2ae39b50f71ac3712953
Igor Fedotov <jus1no1hing@gmail.com>
2020-04-08 22:47:37 +0200

    added cpp proj

---
+++ infm/infm/main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  infm
+//
+//  Created by Igor on 08.04.2020.
+//  Copyright © 2020 Fedotov Igor. All rights reserved.
+//
+
--- infm_mega2560/infm_mega2560.ino
+++ infm_mega2560/infm_mega2560.ino
@@ -1,3 +1,5 @@
+#include <FastLED.h>
+
 void setup() {
   // put your setup code here, to run once:
 
